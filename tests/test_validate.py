b5c2c70ebddc9e8ce328d32e9df2de7215ee7c92
Ferran Llamas <llamas.arroniz@gmail.com>
2020-10-29 09:17:37 +0100

    support integers schema type

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -20,12 +20,35 @@ def test_validate_from_env(env):
     enviral.validate_env(schema)
 
 
-def test_validate_int_from_env(env):
+def test_validate_number_from_env(env):
     os.environ["FOO"] = "bar"
     schema = {"type": "object", "properties": {"foo": {"type": "number"}}}
     with pytest.raises(jsonschema.exceptions.ValidationError):
         enviral.validate_env(schema)
 
+    # Check int
     os.environ["FOO"] = "2"
     settings = enviral.validate_env(schema)
     assert settings["foo"] == 2
+
+    # Check float
+    os.environ["FOO"] = "2.25"
+    settings = enviral.validate_env(schema)
+    assert settings["foo"] == 2.25
+
+
+def test_validate_integer_from_env(env):
+    os.environ["FOO"] = "bar"
+    schema = {"type": "object", "properties": {"foo": {"type": "integer"}}}
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        enviral.validate_env(schema)
+
+    # Check int
+    os.environ["FOO"] = "50"
+    settings = enviral.validate_env(schema)
+    assert settings["foo"] == 50
+
+    # Float should fail
+    os.environ["FOO"] = "2.25"
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        enviral.validate_env(schema)
